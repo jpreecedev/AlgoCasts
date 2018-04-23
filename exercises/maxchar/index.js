5c2649778e9a1fabbd2a074e83d46cc86418e86b
Jon Preece <jonpreece@hotmail.co.uk>
2018-04-23 15:18:44 +0100

    Maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charMap = {}
+  let maxChar = ''
+  let max = 0
 
-module.exports = maxChar;
+  for (let char of str) {
+    if (!charMap[char]) {
+      charMap[char] = 1
+    } else {
+      charMap[char]++
+    }
+
+    if (charMap[char] > max) {
+      maxChar = char
+      max = charMap[char]
+    }
+  }
+
+  return maxChar
+}
+
+module.exports = maxChar
