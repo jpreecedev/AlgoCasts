8e1109b4a1339b1af3784f2c2dcee169bde6e5c4
Jon Preece <jonpreece@hotmail.co.uk>
2018-04-23 14:02:23 +0100

    Reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversed = parseInt(
+    n
+      .toString()
+      .split('')
+      .reverse()
+      .join('')
+  )
 
-module.exports = reverseInt;
+  return reversed * Math.sign(n)
+}
+
+module.exports = reverseInt
